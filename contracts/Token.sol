dbdccdd401d142edee968b425705245bb4c89812
kingcocomango <ahmed.ymg.ali@gmail.com>
2017-10-19 22:22:10 +0200

    Changed token symbol to MOAT

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -5,6 +5,6 @@ import "./zeppelin/PausableToken.sol";
 
 contract Token is MintableToken, PausableToken{
 	string public constant name = "MOAT";
-	string public constant symbol = "MAT";
+	string public constant symbol = "MOAT";
 	uint8 public constant decimals = 18;
 }
