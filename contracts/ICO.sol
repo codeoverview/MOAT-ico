47cc26ac494d12942292dc53c841905f99d01296
kingcocomango <ahmed.ymg.ali@gmail.com>
2017-12-07 15:23:52 +0100

    Added decimals to initial distribution

--- contracts/ICO.sol
+++ contracts/ICO.sol
@@ -97,15 +97,15 @@ contract ICO is Ownable {
         endTimePre = _startTimePre + 12 days;
         wallet = _wallet;
 
-        token.mint(0x8Faa64B6b2eD30290554128289f3A6De9A97D8F6, 4675000000);
-        token.mint(0x2ef6aB856a6be9220A0Cf3Be798b3E05fA29267F, 225000000);
-        token.mint(0xe84f002ED596E38D7f1cE048503b13321eb28A98, 300000000);
+        token.mint(0x8Faa64B6b2eD30290554128289f3A6De9A97D8F6, 4675000000 * 10**18);
+        token.mint(0x2ef6aB856a6be9220A0Cf3Be798b3E05fA29267F, 225000000 * 10**18);
+        token.mint(0xe84f002ED596E38D7f1cE048503b13321eb28A98, 300000000 * 10**18);
 
 
-        token.mint(0xB4EB582b0055d9f8B8ad862292cA1b33dfE8215C, 100000000);
-        token.mint(0xd6f13F05DBB959f8DAA6721a088906Fef4Ad093c, 500000000);
-        token.mint(0x220Ea3406b1b9d72B6386EA29EfF73a230D5d51c, 700000000);
-        token.mint(0x87969413c2388B23c2ac871a61702d1b2d67b9CB, 2000000000);
+        token.mint(0xB4EB582b0055d9f8B8ad862292cA1b33dfE8215C, 100000000 * 10**18);
+        token.mint(0xd6f13F05DBB959f8DAA6721a088906Fef4Ad093c, 500000000 * 10**18);
+        token.mint(0x220Ea3406b1b9d72B6386EA29EfF73a230D5d51c, 700000000 * 10**18);
+        token.mint(0x87969413c2388B23c2ac871a61702d1b2d67b9CB, 2000000000 * 10**18);
         token.pause(); // Unpause after ICO is over
     }
 
